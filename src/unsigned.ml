yallop/ocaml-ctypes

Add to_int64 to the Unsigned.S interface.

src/ctypes/unsigned.ml
@@ -22,6 +22,7 @@ module type Basics = sig
   val shift_right : t -> int -> t
   val of_int : int -> t
   val to_int : t -> int
+  val to_int64 : t -> int64
   val of_string : string -> t
   val to_string : t -> string
 end
@@ -107,6 +108,7 @@ struct
     external shift_right : t -> int -> t = "ctypes_uint8_shift_right"
     external of_int : int -> t = "ctypes_uint8_of_int"
     external to_int : t -> int = "ctypes_uint8_to_int"
+    external to_int64 : t -> int64 = "ctypes_uint8_to_int64"
     external of_string : string -> t = "ctypes_uint8_of_string"
     external to_string : t -> string = "ctypes_uint8_to_string"
     external _max_int : unit -> t = "ctypes_uint8_max"
@@ -135,6 +137,7 @@ struct
     external shift_right : t -> int -> t = "ctypes_uint16_shift_right"
     external of_int : int -> t = "ctypes_uint16_of_int"
     external to_int : t -> int = "ctypes_uint16_to_int"
+    external to_int64 : t -> int64 = "ctypes_uint16_to_int64"
     external of_string : string -> t = "ctypes_uint16_of_string"
     external to_string : t -> string = "ctypes_uint16_to_string"
     external _max_int : unit -> t = "ctypes_uint16_max"
@@ -167,6 +170,7 @@ struct
     external shift_right : t -> int -> t = "ctypes_uint32_shift_right"
     external of_int : int -> t = "ctypes_uint32_of_int"
     external to_int : t -> int = "ctypes_uint32_to_int"
+    external to_int64 : t -> int64 = "ctypes_uint32_to_int64"
     external of_string : string -> t = "ctypes_uint32_of_string"
     external to_string : t -> string = "ctypes_uint32_to_string"
     external _max_int : unit -> t = "ctypes_uint32_max"
@@ -183,7 +187,7 @@ end
 module UInt64 : sig
   include S
   external of_int64 : int64 -> t = "ctypes_uint64_of_int64"
-  external to_int64 : t -> int64 = "ctypes_int64_of_uint64"
+  external to_int64 : t -> int64 = "ctypes_uint64_to_int64"
 end = 
 struct
   module B =
@@ -201,6 +205,7 @@ struct
     external shift_right : t -> int -> t = "ctypes_uint64_shift_right"
     external of_int : int -> t = "ctypes_uint64_of_int"
     external to_int : t -> int = "ctypes_uint64_to_int"
+    external to_int64 : t -> int64 = "ctypes_uint64_to_int64"
     external of_string : string -> t = "ctypes_uint64_of_string"
     external to_string : t -> string = "ctypes_uint64_to_string"
     external _max_int : unit -> t = "ctypes_uint64_max"
@@ -210,7 +215,6 @@ struct
   include Extras(B)
   module Infix = MakeInfix(B)
   external of_int64 : int64 -> t = "ctypes_uint64_of_int64"
-  external to_int64 : t -> int64 = "ctypes_int64_of_uint64"
 end
 
 

src/ctypes/unsigned_stubs.c
@@ -130,6 +130,12 @@
     return Val_int(Uint_custom_val(TYPE(BITS), a));                          \
   }                                                                          \
                                                                              \
+  /* to_int64 : t -> int64 */                                                \
+  value ctypes_uint ## BITS ## _to_int64(value a)                            \
+  {                                                                          \
+    return caml_copy_int64(Uint_custom_val(TYPE(BITS), a));                  \
+  }                                                                          \
+                                                                             \
   /* of_string : string -> t */                                              \
   value ctypes_uint ## BITS ## _of_string(value a)                           \
   {                                                                          \
@@ -170,7 +176,6 @@ value ctypes_ulonglong_size (value _) { return Val_int(sizeof (unsigned long lon
 value ctypes_uint32_of_int32 (value i) { return ctypes_copy_uint32(Int32_val(i)); }
 value ctypes_int32_of_uint32 (value u) { return caml_copy_int32(Uint_custom_val(uint32_t, u)); }
 value ctypes_uint64_of_int64 (value i) { return ctypes_copy_uint64(Int64_val(i)); }
-value ctypes_int64_of_uint64 (value u) { return caml_copy_int64(Uint_custom_val(uint64_t, u)); }
 value ctypes_uintptr_t_size (value _) { return Val_int(sizeof (uintptr_t)); }
 value ctypes_intptr_t_size (value _) { return Val_int(sizeof (intptr_t)); }
 value ctypes_ptrdiff_t_size (value _) { return Val_int(sizeof (ptrdiff_t)); }

yallop/ocaml-integers

src/unsigned.ml
@@ -22,6 +22,7 @@ module type Basics = sig
   val shift_right : t -> int -> t
   val of_int : int -> t
   val to_int : t -> int
+  val to_int64 : t -> int64
   val of_string : string -> t
   val to_string : t -> string
 end
@@ -107,6 +108,7 @@ struct
     external shift_right : t -> int -> t = "integers_uint8_shift_right"
     external of_int : int -> t = "integers_uint8_of_int"
     external to_int : t -> int = "integers_uint8_to_int"
+    external to_int64 : t -> int64 = "integers_uint8_to_int64"
     external of_string : string -> t = "integers_uint8_of_string"
     external to_string : t -> string = "integers_uint8_to_string"
     external _max_int : unit -> t = "integers_uint8_max"
@@ -135,6 +137,7 @@ struct
     external shift_right : t -> int -> t = "integers_uint16_shift_right"
     external of_int : int -> t = "integers_uint16_of_int"
     external to_int : t -> int = "integers_uint16_to_int"
+    external to_int64 : t -> int64 = "integers_uint16_to_int64"
     external of_string : string -> t = "integers_uint16_of_string"
     external to_string : t -> string = "integers_uint16_to_string"
     external _max_int : unit -> t = "integers_uint16_max"
@@ -167,6 +170,7 @@ struct
     external shift_right : t -> int -> t = "integers_uint32_shift_right"
     external of_int : int -> t = "integers_uint32_of_int"
     external to_int : t -> int = "integers_uint32_to_int"
+    external to_int64 : t -> int64 = "integers_uint32_to_int64"
     external of_string : string -> t = "integers_uint32_of_string"
     external to_string : t -> string = "integers_uint32_to_string"
     external _max_int : unit -> t = "integers_uint32_max"
@@ -183,7 +187,7 @@ end
 module UInt64 : sig
   include S
   external of_int64 : int64 -> t = "integers_uint64_of_int64"
-  external to_int64 : t -> int64 = "integers_int64_of_uint64"
+  external to_int64 : t -> int64 = "integers_uint64_to_int64"
 end = 
 struct
   module B =
@@ -201,6 +205,7 @@ struct
     external shift_right : t -> int -> t = "integers_uint64_shift_right"
     external of_int : int -> t = "integers_uint64_of_int"
     external to_int : t -> int = "integers_uint64_to_int"
+    external to_int64 : t -> int64 = "integers_uint64_to_int64"
     external of_string : string -> t = "integers_uint64_of_string"
     external to_string : t -> string = "integers_uint64_to_string"
     external _max_int : unit -> t = "integers_uint64_max"
@@ -210,7 +215,6 @@ struct
   include Extras(B)
   module Infix = MakeInfix(B)
   external of_int64 : int64 -> t = "integers_uint64_of_int64"
-  external to_int64 : t -> int64 = "integers_int64_of_uint64"
 end
 
 

src/unsigned_stubs.c
@@ -130,6 +130,12 @@
     return Val_int(Uint_custom_val(TYPE(BITS), a));                          \
   }                                                                          \
                                                                              \
+  /* to_int64 : t -> int64 */                                                \
+  value integers_uint ## BITS ## _to_int64(value a)                          \
+  {                                                                          \
+    return caml_copy_int64(Uint_custom_val(TYPE(BITS), a));                  \
+  }                                                                          \
+                                                                             \
   /* of_string : string -> t */                                              \
   value integers_uint ## BITS ## _of_string(value a)                         \
   {                                                                          \
@@ -170,7 +176,6 @@ value integers_ulonglong_size (value _) { return Val_int(sizeof (unsigned long l
 value integers_uint32_of_int32 (value i) { return integers_copy_uint32(Int32_val(i)); }
 value integers_int32_of_uint32 (value u) { return caml_copy_int32(Uint_custom_val(uint32_t, u)); }
 value integers_uint64_of_int64 (value i) { return integers_copy_uint64(Int64_val(i)); }
-value integers_int64_of_uint64 (value u) { return caml_copy_int64(Uint_custom_val(uint64_t, u)); }
 value integers_uintptr_t_size (value _) { return Val_int(sizeof (uintptr_t)); }
 value integers_intptr_t_size (value _) { return Val_int(sizeof (intptr_t)); }
 value integers_ptrdiff_t_size (value _) { return Val_int(sizeof (ptrdiff_t)); }