yallop/ocaml-ctypes

Add support for ptrdiff_t as a typedef for a signed integer type.

src/ctypes/unsigned_stubs.c
@@ -173,3 +173,4 @@ value ctypes_uint64_of_int64 (value i) { return ctypes_copy_uint64(Int64_val(i))
 value ctypes_int64_of_uint64 (value u) { return caml_copy_int64(Uint_custom_val(uint64_t, u)); }
 value ctypes_uintptr_t_size (value _) { return Val_int(sizeof (uintptr_t)); }
 value ctypes_intptr_t_size (value _) { return Val_int(sizeof (intptr_t)); }
+value ctypes_ptrdiff_t_size (value _) { return Val_int(sizeof (ptrdiff_t)); }

yallop/ocaml-integers

src/unsigned_stubs.c
@@ -173,4 +173,5 @@ value integers_uint64_of_int64 (value i) { return integers_copy_uint64(Int64_val
 value integers_int64_of_uint64 (value u) { return caml_copy_int64(Uint_custom_val(uint64_t, u)); }
 value integers_uintptr_t_size (value _) { return Val_int(sizeof (uintptr_t)); }
 value integers_intptr_t_size (value _) { return Val_int(sizeof (intptr_t)); }
+value integers_ptrdiff_t_size (value _) { return Val_int(sizeof (ptrdiff_t)); }
 