yallop/ocaml-ctypes

Add new "sint" primitive type for full-range C's int

src/ctypes/signed.ml
@@ -109,6 +109,7 @@ struct
 end
 
 (* C guarantees that sizeof(t) == sizeof(unsigned t) *)
+external int_size : unit -> int = "ctypes_uint_size"
 external long_size : unit -> int = "ctypes_ulong_size"
 external llong_size : unit -> int = "ctypes_ulonglong_size"
 
@@ -118,8 +119,10 @@ let pick : size:int -> (module S) =
     | 8 -> (module Int64)
     | _ -> assert false
 
+module SInt = (val pick ~size:(int_size ()))
 module Long = (val pick ~size:(long_size ()))
 module LLong = (val pick ~size:(llong_size ()))
 
+type sint = SInt.t
 type long = Long.t
 type llong = LLong.t

yallop/ocaml-integers

src/signed.ml
@@ -109,8 +109,9 @@ struct
 end
 
 (* C guarantees that sizeof(t) == sizeof(unsigned t) *)
-external long_size : unit -> int = "ctypes_ulong_size"
-external llong_size : unit -> int = "ctypes_ulonglong_size"
+external int_size : unit -> int = "integers_uint_size"
+external long_size : unit -> int = "integers_ulong_size"
+external llong_size : unit -> int = "integers_ulonglong_size"
 
 let pick : size:int -> (module S) =
   fun ~size -> match size with
@@ -118,8 +119,10 @@ let pick : size:int -> (module S) =
     | 8 -> (module Int64)
     | _ -> assert false
 
+module SInt = (val pick ~size:(int_size ()))
 module Long = (val pick ~size:(long_size ()))
 module LLong = (val pick ~size:(llong_size ()))
 
+type sint = SInt.t
 type long = Long.t
 type llong = LLong.t