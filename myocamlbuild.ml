305b5e64fc6a2ea5c018f9d458d7c74f665c4d48
Jeremy Yallop <yallop@gmail.com>
2016-09-19 11:40:25 +0100

    Add build and packaging infrastructure.

---
+++ myocamlbuild.ml
@@ -0,0 +1,20 @@
+open Ocamlbuild_plugin
+
+let () = dispatch begin function
+    | After_rules ->
+      dep ["c"; "compile"; "use_unsigned_headers"] ["src/unsigned_stubs.h"];
+
+      flag ["use_integer_stubs"] &
+        S[A"-I"; A"src"];
+
+      dep ["ocaml"; "link"; "byte"; "library"; "use_integer_stubs"]
+        ["src/dllintegers"-.-(!Options.ext_dll)];
+      flag ["ocaml"; "link"; "byte"; "library"; "use_integer_stubs"] &
+        S[A"-dllib"; A"-lintegers"];
+
+      dep ["ocaml"; "link"; "native"; "library"; "use_integer_stubs"]
+        ["src/libintegers"-.-(!Options.ext_lib)];
+      flag ["ocaml"; "link"; "native"; "library"; "use_integer_stubs"] &
+        S[A"-cclib"; A"-lintegers"];
+    | _ -> ()
+end
---
+++ pkg/pkg.ml
@@ -0,0 +1,10 @@
+#!/usr/bin/env ocaml
+#use "topfind"
+#require "topkg"
+open Topkg
+
+let () =
+  Pkg.describe "integers" @@ fun c ->
+  Ok [ Pkg.mllib "src/integers.mllib";
+       Pkg.clib "src/libintegers.clib";
+       Pkg.mllib "top/integer_printers.mllib"; ]
