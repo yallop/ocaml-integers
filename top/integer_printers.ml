0e33c3cbc8f84c4b79c64d26c5e47f0e53af5d3f
Rudi Grinberg <rudi.grinberg@gmail.com>
2018-12-07 06:04:46 +0000

    Remove wrapped false
    Signed-off-by: Rudi Grinberg <rudi.grinberg@gmail.com>

--- top/integer_printers.ml
+++ top/integer_printers.ml
@@ -6,26 +6,26 @@
  *)
 
 let format_sint fmt v =
-  Format.fprintf fmt "<sint %s>" (Integers.Signed.SInt.to_string v)
+  Format.fprintf fmt "<sint %s>" (Signed.SInt.to_string v)
 let format_long fmt v =
-  Format.fprintf fmt "<long %s>" (Integers.Signed.Long.to_string v)
+  Format.fprintf fmt "<long %s>" (Signed.Long.to_string v)
 let format_llong fmt v =
-  Format.fprintf fmt "<llong %s>" (Integers.Signed.LLong.to_string v)
+  Format.fprintf fmt "<llong %s>" (Signed.LLong.to_string v)
 let format_uchar fmt v =
-  Format.fprintf fmt "<uchar %s>" (Integers.Unsigned.UChar.to_string v)
+  Format.fprintf fmt "<uchar %s>" (Unsigned.UChar.to_string v)
 let format_uint8 fmt v =
-  Format.fprintf fmt "<uint8 %s>" (Integers.Unsigned.UInt8.to_string v)
+  Format.fprintf fmt "<uint8 %s>" (Unsigned.UInt8.to_string v)
 let format_uint16 fmt v =
-  Format.fprintf fmt "<uint16 %s>" (Integers.Unsigned.UInt16.to_string v)
+  Format.fprintf fmt "<uint16 %s>" (Unsigned.UInt16.to_string v)
 let format_uint32 fmt v =
-  Format.fprintf fmt "<uint32 %s>" (Integers.Unsigned.UInt32.to_string v)
+  Format.fprintf fmt "<uint32 %s>" (Unsigned.UInt32.to_string v)
 let format_uint64 fmt v =
-  Format.fprintf fmt "<uint64 %s>" (Integers.Unsigned.UInt64.to_string v)
+  Format.fprintf fmt "<uint64 %s>" (Unsigned.UInt64.to_string v)
 let format_ushort fmt v =
-  Format.fprintf fmt "<ushort %s>" (Integers.Unsigned.UShort.to_string v)
+  Format.fprintf fmt "<ushort %s>" (Unsigned.UShort.to_string v)
 let format_uint fmt v =
-  Format.fprintf fmt "<uint %s>" (Integers.Unsigned.UInt.to_string v)
+  Format.fprintf fmt "<uint %s>" (Unsigned.UInt.to_string v)
 let format_ulong fmt v =
-  Format.fprintf fmt "<ulong %s>" (Integers.Unsigned.ULong.to_string v)
+  Format.fprintf fmt "<ulong %s>" (Unsigned.ULong.to_string v)
 let format_ullong fmt v =
-  Format.fprintf fmt "<ullong %s>" (Integers.Unsigned.ULLong.to_string v)
+  Format.fprintf fmt "<ullong %s>" (Unsigned.ULLong.to_string v)
--- top/integer_printers.mli
+++ top/integer_printers.mli
@@ -7,15 +7,15 @@
 
 open Format
 
-val format_sint : formatter -> Integers.Signed.SInt.t -> unit
-val format_long : formatter -> Integers.Signed.Long.t -> unit
-val format_llong : formatter -> Integers.Signed.LLong.t -> unit
-val format_uchar : formatter -> Integers.Unsigned.UChar.t -> unit
-val format_uint8 : formatter -> Integers.Unsigned.UInt8.t -> unit
-val format_uint16 : formatter -> Integers.Unsigned.UInt16.t -> unit
-val format_uint32 : formatter -> Integers.Unsigned.UInt32.t -> unit
-val format_uint64 : formatter -> Integers.Unsigned.UInt64.t -> unit
-val format_ushort : formatter -> Integers.Unsigned.UShort.t -> unit
-val format_uint : formatter -> Integers.Unsigned.UInt.t -> unit
-val format_ulong : formatter -> Integers.Unsigned.ULong.t -> unit
-val format_ullong : formatter -> Integers.Unsigned.ULLong.t -> unit
+val format_sint : formatter -> Signed.SInt.t -> unit
+val format_long : formatter -> Signed.Long.t -> unit
+val format_llong : formatter -> Signed.LLong.t -> unit
+val format_uchar : formatter -> Unsigned.UChar.t -> unit
+val format_uint8 : formatter -> Unsigned.UInt8.t -> unit
+val format_uint16 : formatter -> Unsigned.UInt16.t -> unit
+val format_uint32 : formatter -> Unsigned.UInt32.t -> unit
+val format_uint64 : formatter -> Unsigned.UInt64.t -> unit
+val format_ushort : formatter -> Unsigned.UShort.t -> unit
+val format_uint : formatter -> Unsigned.UInt.t -> unit
+val format_ulong : formatter -> Unsigned.ULong.t -> unit
+val format_ullong : formatter -> Unsigned.ULLong.t -> unit
